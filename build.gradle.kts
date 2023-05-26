a29000db6a4adc2e4c657bb6c927063b6bd1a0ec
kaiqkt <kaiquebeserra1313@gmail.com>
2023-05-25 22:17:50 -0300

    spring config

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 allprojects {
 
     group = "com.kaiqkt"
-    version = "1.0.5"
+    version = "1.0.6"
 
     apply(plugin = "org.jetbrains.kotlin.jvm")
     apply(plugin = "maven-publish")
