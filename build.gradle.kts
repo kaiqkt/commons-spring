4de9c3e42e4a40eb0752f5549457f2afe8f31f47
kaiqkt <kaiquebeserra1313@gmail.com>
2023-09-01 00:35:29 -0300

    feat: changed spring boot version

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 allprojects {
 
     group = "com.kaiqkt"
-    version = "1.4.0"
+    version = "1.4.1"
 
     apply(plugin = "org.jetbrains.kotlin.jvm")
     apply(plugin = "maven-publish")
