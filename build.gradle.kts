827465a7475aca6651fea39dadaaea247900990f
kaiqkt <kaiquebeserra1313@gmail.com>
2023-12-11 00:41:20 -0300

    feat: change to spring 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 allprojects {
 
     group = "com.kaiqkt"
-    version = "1.4.5"
+    version = "2.0.0"
 
     apply(plugin = "org.jetbrains.kotlin.jvm")
     apply(plugin = "maven-publish")
